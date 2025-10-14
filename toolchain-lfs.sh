30baba028be41b6b1d8f8236b815fb6b70a26ec7
alexgeiser78 <alexgeiser78@gmail.com>
2025-10-14 17:12:17 +0200

    last check

--- toolchain-lfs.sh
+++ toolchain-lfs.sh
@@ -28,7 +28,6 @@ if [ "$(id -u)" -eq 0 ]; then
 
     echo ">> Switching to user 'lfs'..."
     exec su - lfs
-    cd /usr/src/ft-linux
 fi
 
 # --------------------------------------------------------
@@ -124,9 +123,9 @@ rm -rf binutils-*/
 echo
 echo "✅ Binutils Pass 1 built successfully!"
 echo "Checking installed binaries:"
-ls -l $LFS/tools/bin/x86_64-lfs-linux-gnu-ar --version | head -n1
-ls -l $LFS/tools/bin/x86_64-lfs-linux-gnu-as --version | head -n1
-ls -l $LFS/tools/bin/x86_64-lfs-linux-gnu-ld --version | head -n1
+$LFS/tools/bin/x86_64-lfs-linux-gnu-ar --version | head -n1
+$LFS/tools/bin/x86_64-lfs-linux-gnu-as --version | head -n1
+$LFS/tools/bin/x86_64-lfs-linux-gnu-ld --version | head -n1
 
 
 echo
