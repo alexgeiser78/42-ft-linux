5f5fc48393a5f13d9b6feb2bb6527634b340e347
alexgeiser78 <alexgeiser78@gmail.com>
2025-10-17 18:57:03 +0200

    share gcc

---
+++ share.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+export LFS=/mnt/lfs
+export LFS_TGT=$(uname -m)-lfs-linux-gnu
+export PATH=$LFS/tools/bin:$PATH
+export LC_ALL=POSIX
+
+cd $LFS/sources
+cd gcc-*/
+
+
+
+../configure                  \
+    --target=$LFS_TGT         \
+    --prefix=$LFS/tools       \
+    --with-glibc-version=2.42 \
+    --with-sysroot=$LFS       \
+    --with-newlib             \
+    --without-headers         \
+    --enable-default-pie      \
+    --enable-default-ssp      \
+    --disable-nls             \
+    --disable-shared          \
+    --disable-multilib        \
+    --disable-threads         \
+    --disable-libatomic       \
+    --disable-libgomp         \
+    --disable-libquadmath     \
+    --disable-libssp          \
+    --disable-libvtv          \
+    --disable-libstdcxx       \
+    --enable-languages=c,c++
